9d11cb7fe3d7d03f1093bb07c5119658c4c84f8f
bethelmyd <bethelmyd@gmail.com>
2016-10-11 22:59:04 -0400

    last changes just before deadline

--- assets/js/psychic.js
+++ assets/js/psychic.js
@@ -4,88 +4,88 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var letters = "abcdefghijklmnopqrstuvwxyz";
-//var letters = "ab";
+// var letters = "ab";
 var computerGuess = "";
+var userGuess = "";
+var gameOn = false;
 
 document.onkeydown = function(event)
 {
-	if(guessesLeft == 0)
-	{
-		reset();		
+	if (!gameOn){
+		gameOn = true;
+		reset();
+		getComputerGuess();
+		document.querySelector("#results").innerHTML = "";
 	}
-	else
-	{
-		var userGuess = getUserGuess(event);
-		computerGuess = getComputerGuess();
-		seeWhoWonRound(userGuess, computerGuess);
-		if(guessesLeft == 0)
+	else{
+		getUserGuess(event);
+		if(userGuess == "") return;  //guessed already
+		if(userGuess == computerGuess)
 		{
-			seeWhoWon();
+			wins++;
+			gameOn = false;
+			showSummary();
+		}
+		else
+		{
+			guessesLeft--;
+			document.querySelector("#guessesLeft").innerHTML = guessesLeft;	
+			if(guessesLeft == 0)
+			{
+				losses++;
+				gameOn = false;
+				showSummary();
+			}
+
 		}
 	}
 }
 
+
 function getUserGuess(event)
 {
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	if(userGuess < 'a' || userGuess > 'z' || guesses.indexOf(userGuess) >= 0){
+		userGuess = "";
+		return;
+	}
 	guesses += userGuess + " ";
 	document.querySelector("#guesses").innerHTML = guesses;
-	return userGuess;
 }
 
 function getComputerGuess()
 {
 	var computerRandomNumber = Math.floor(Math.random() * letters.length);
-	var computerGuess = letters.charAt(computerRandomNumber);
-	document.querySelector("#computerGuess").innerHTML = computerGuess;	
-	return computerGuess;		
+	computerGuess = letters.charAt(computerRandomNumber);
+	// document.querySelector("#computerGuess").innerHTML = computerGuess;		
 }
 
-function seeWhoWonRound(userGuess, computerGuess)
+function showSummary()
 {
-	if(userGuess == computerGuess)
-	{
-		wins++;
-	}
-	else
-	{
-		losses++;
-	}
-
-	guessesLeft--;
-
-	document.querySelector("#wins").innerHTML = wins;	
-	document.querySelector("#losses").innerHTML = losses;	
-	document.querySelector("#guessesLeft").innerHTML = guessesLeft;	
-}
-
-function seeWhoWon()
-{
-	if(wins > losses)
+	if(guessesLeft > 0)
 	{
 		document.querySelector("#results").innerHTML = "<p>Congrats! You won!</p><p>Press any key to play again.</p>";	
 	}
-	else if (losses > wins)
+	else
 	{
 		document.querySelector("#results").innerHTML = "<p>Sorry! You lost!</p><p>Press any key to play again.</p>";	
 	}
-	else
-	{
-		document.querySelector("#results").innerHTML = "<p>Looks like we tied.</p><p>Press any key to play again.</p>";	
-	}
+	document.querySelector("#computerGuess").innerHTML = computerGuess;	
+	document.querySelector("#wins").innerHTML = wins;	
+	document.querySelector("#losses").innerHTML = losses;	
 }
 
 function reset()
 {
 	guesses = "";
-	wins = 0;
-	losses = 0;
+	// wins = 0;
+	// losses = 0;
 	guessesLeft = 10;
 	computerGuess = "";
 	document.querySelector("#guesses").innerHTML = guesses;	
-	document.querySelector("#wins").innerHTML = wins;	
-	document.querySelector("#losses").innerHTML = losses;	
+	// document.querySelector("#wins").innerHTML = wins;	
+	// document.querySelector("#losses").innerHTML = losses;	
 	document.querySelector("#guessesLeft").innerHTML = guessesLeft;	
 	document.querySelector("#computerGuess").innerHTML = computerGuess;	
-	document.querySelector("#results").innerHTML = "";
+	document.querySelector("#results").innerHTML = "Press any key to start.";
 }
